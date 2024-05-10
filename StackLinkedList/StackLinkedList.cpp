b6cb20e7f92730dd274c73391e3b305527754b51
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-10 10:42:44 +0700

    push value ke dalam stack

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -69,5 +69,11 @@ int main() {
 		cout << "Enter your choice : ";
 		cin >> choice;
 
+		switch (choice) {
+		case 1:
+			cout << "Enter the value to push : ";
+			cin >> value;
+			stack.push(value);
+			break;
 	}
 }
