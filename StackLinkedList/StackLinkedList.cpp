4b2bda6f03c55851b0105547650db86fc210a201
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-10 10:42:19 +0700

    int main dengan switch case

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -55,3 +55,19 @@ public:
 		return top == NULL;
 	}
 };
+
+int main() {
+	stack stack;
+	int choice = 0;
+	int value;
+
+	while (choice != 5) {
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3.Peek\n";
+		cout << "Exit\n";
+		cout << "Enter your choice : ";
+		cin >> choice;
+
+	}
+}
