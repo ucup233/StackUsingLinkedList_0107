498c03e66122217923f841a9e9940b72a6964cdd
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-10 10:36:01 +0700

    prosedur pop (untuk menghapus elemen top dari stack)

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -25,5 +25,15 @@ public:
 		newNode->data = value;
 		newNode->next = top;
 		top = newNode;
+		cout << "Push value : " << value << endl;
+		return value;
+	}
+
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value : " << top->data << endl;
+	
 	}
 };
